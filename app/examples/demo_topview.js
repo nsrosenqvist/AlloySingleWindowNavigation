f5e3adff434e7656e754b8733d7eaa819e2bec6b
Niklas Rosenqvist <niklas.s.rosenqvist@gmail.com>
2013-04-05 17:15:53 +0200

    Corrected subview-title when opened from topview

--- app/examples/demo_topview.js
+++ app/examples/demo_topview.js
@@ -1,5 +1,5 @@
 var navigation = Alloy.Globals.navigation;
 
 $.button.addEventListener("click", function() {
-	navigation.open("demo_subview", {title: "Open subview"});
+	navigation.open("demo_subview", {title: "Subview"});
 });
